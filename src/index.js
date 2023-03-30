89a16ee4b192c878190e98cea3f9800f42a8859e
Ryan <ryanbiy01@gmail.com>
2023-03-30 18:27:19 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  document.querySelector('form').addEventListener
+  ('submit',(e)=>{
+    e.preventDefault()
+    newTask(e.target.new_task.value)
+    // newTask(e.target.new_task.value)
+  })
 });
+
+ function newTask(content){
+  let ul=document.getElementById('tasks')
+  let li=document.createElement('li')
+  ul.appendChild(li)
+  li.innerHTML=`${ content}`;
+  let btn=document.createElement('button');
+  btn.addEventListener( 'click',removeToDolist)
+  btn.innerHTML= 'X'
+  li.appendChild(btn)
+ } 
+ function removeToDolist(e){ 
+  e.target.parentNode.remove()
+ }
